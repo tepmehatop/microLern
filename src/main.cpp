a4e7aa30fa6ebd7baf78fed7b5f714916c789da6
Mark Nadelman <bmwark@gmail.com>
2019-10-09 09:40:54 +0300

    init

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#define F_CPU 1000000UL 
+
+#include <avr/io.h>
+#include <util/delay.h>
+int count = 0;
+
+int arr[10] ={0b00000110, 0b01011011, 0b01001111, 0b01100110, 0b01101101, 0b01111101, 0b00000111, 0b01111111, 0b01101111, 0b00111111}; 
+//               1          2             3          4           5             6         7 
+int main(void)
+
+{
+	
+	DDRB = 0b1111111;
+	
+    /* Replace with your application code */
+    while (1) 
+    {
+        for(int i = 0; i < 10; i++ ){
+        PORTB = arr[i];
+        _delay_ms(200);
+        }
+    }
+}
+ 
