634a65847a1d23986988023c1c072d77ab995515
Mark Nadelman <bmwark@gmail.com>
2019-10-10 01:44:06 +0300

    fix pinD

--- src/main.cpp
+++ src/main.cpp
@@ -11,19 +11,19 @@ int main(void)
 {
 	
 	DDRB = 0b1111111;
-    DDRD = 0b1111111;
+    DDRD =  0b0000000;
     PORTD = 0b1111111;
 	
     /* Replace with your application code */
     while (1) 
     {
-        if (PORTD == 0b1111110){
+        if (PIND == 0b1111110){
         for(int i = 0; i < 10; i++ ){
         PORTB = arr[i];
         _delay_ms(200);
         }
         }
-        else if (PORTD == 0b1111101)
+        else if (PIND == 0b1111101)
         {
         for(int i = 10; i > 10; i-- ){
         PORTB = arr[i];
